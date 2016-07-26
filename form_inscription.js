eca079fa451565e4d32cb33257cee7bf9915b7ed
Kévin TRINCHE <Kvin TRINCHE>
2016-07-26 16:33:27 +0200

    MODIF JAVASCRIPT INSCRIPTION

--- form_inscription.js
+++ form_inscription.js
@@ -1,4 +1,4 @@
-function estMail(champ, messageAlerte)	{
+function estMail(champ)	{
 /* Fonction de test de saisie numérique*/
 	var email = new RegExp('^[0-9a-z._-]+@{1}[0-9a-z.-]{2,}[.]{1}[a-z]{2,5}$');
 	if(champ.value.match(email))	{
@@ -6,19 +6,59 @@ function estMail(champ, messageAlerte)	{
 		return true;
 	}
 	else {
-		alert(messageAlerte);
 		//Focus sur le champ en erreur
 		champ.focus();
 		return false;
 	}
 }
 
+function estNom(champ)	{
+	var nom = /^[a-zA-Z]+$/;
+	if(champ.value.match(nom))	{
+		return true;
+	}
+	else {
+		champ.focus();
+		return false;
+	}
+}
 
-function estCorrect(champ1,champ2,champ3,champ4) 
+function estPrenom(champ)	{
+/* Fonction de test de saisie numérique*/
+	var prenom = /^[a-zA-Z]+$/;
+	if(champ.value.match(prenom))	{
+		// alert("Vous avez saisi " + document.getElementById('email').value);
+		return true;
+	}
+	else {
+		//Focus sur le champ en erreur
+		champ.focus();
+		return false;
+	}
+}
+
+function estCorrect(champ1,champ2,champ3) 
 {
-    if(estmail(email,'Veuillez saisir un email correct') && estNom(nom,'Veuillez saisir un nom correct') && estPrenom(prenom,'Veuillez saisir un nom correct') && estAge(age,'Veuillez saisir un nom correct'))
+    if(estMail(champ1) && estNom(champ2) && estPrenom(champ3))
     {
-        document.location.href="#"
+		alert("ok");
+        //document.location.href="#";
     }
+	else if (!estMail(champ1))
+	{
+		alert("Veuillez saisir un email correct");
+	}
+	else if (!estNom(champ2))
+	{
+		alert("Veuillez saisir un nom correct");
+	}
+	else if (!estPrenom(champ3))
+	{
+		alert("Veuillez saisir un prénom correct");
+	}
+	/*else if (!estAge(champ4))
+	{
+		alert("Veuillez saisir un age correct");
+	}*/
 
 }
